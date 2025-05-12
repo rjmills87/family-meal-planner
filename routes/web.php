69abb5317ce2b7d36f263d9b9e7f2dc879a3421a
Rhys Miller <rjmills87@aol.com>
2025-05-12 22:40:27 +0100

    feat: add meal plans resource routes with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\MealSuggestionController;
 use App\Http\Controllers\VoteController;
+use App\Http\Controllers\MealPlanController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -24,6 +25,10 @@ Route::middleware(['auth', 'verified'])->group(function() {
     Route::post('/votes', [VoteController::class, 'store'])->name('votes.store');
     Route::delete('/votes/{vote}', [VoteController::class, 'destroy'])->name('votes.destroy');
 });
+
+// Meal Plans Route
+Route::resource('meal-plans', MealPlanController::class)
+->middleware(['auth', 'verified']);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
